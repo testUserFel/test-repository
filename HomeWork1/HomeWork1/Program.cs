0a59288e8f259d9317bc147be39c95aadbd0eca5
testUserFel <93158171+testUserFel@users.noreply.github.com>
2021-10-25 23:05:36 +0300

    update again

--- HomeWork1/HomeWork1/Program.cs
+++ HomeWork1/HomeWork1/Program.cs
@@ -6,7 +6,7 @@ namespace HomeWork1
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!111111");
+      Console.WriteLine("Hello World!111111ghjgh");
     }
   }
 }
